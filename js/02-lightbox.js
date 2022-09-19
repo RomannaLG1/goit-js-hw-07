4f6767fc03a6451931f84cad8fe5a8e74480107b
RomannaLG1 <melnykromasja@gmail.com>
2022-09-19 21:10:01 +0300

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const getGalleryEl = document.querySelector(".gallery");
+
+
+
+const makeGalleryItem = ({ preview, original, description }) => {
+  return `<a class="gallery__item" 
+    href= ${original}>
+  <img class="gallery__image" 
+  src= ${preview} 
+  alt= ${description} />
+</a>`;
+}
+
+const makeGalleryItemMarkup = galleryItems.map(makeGalleryItem).join();
+
+getGalleryEl.insertAdjacentHTML('afterbegin', makeGalleryItemMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlayOpacity: 0.7,
+});
+
+
+
+
